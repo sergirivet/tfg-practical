cdf6fc9cf4f91c3f6d0e2b06e63f00efb7972e27
sergirivet7 <sergi.rivet01@estudiant.upf.edu>
2025-12-31 00:25:02 +0100

    adsas

--- pq_kem/kyber_kem.py
+++ pq_kem/kyber_kem.py
@@ -1,20 +1,16 @@
-import oqs
+from kyber_py.ml_kem import ML_KEM_512
 
 def kyber_keygen():
     """Genera un par de claves Kyber512 (ML-KEM)."""
-    kem = oqs.KeyEncapsulation("Kyber512")
-    public_key = kem.generate_keypair()
-    private_key = kem.export_secret_key()
+    public_key, private_key = ML_KEM_512.keygen()
     return public_key, private_key
 
 def kyber_encapsulate(public_key):
     """Encapsula un secreto usando la clave pública Kyber."""
-    kem = oqs.KeyEncapsulation("Kyber512")
-    ciphertext, shared_secret = kem.encap_secret(public_key)
+    ciphertext, shared_secret = ML_KEM_512.encaps(public_key)
     return ciphertext, shared_secret
 
 def kyber_decapsulate(ciphertext, private_key):
     """Decapsula el secreto usando la clave privada Kyber."""
-    kem = oqs.KeyEncapsulation("Kyber512", private_key)
-    shared_secret = kem.decap_secret(ciphertext)
+    shared_secret = ML_KEM_512.decaps(ciphertext, private_key)
     return shared_secret
